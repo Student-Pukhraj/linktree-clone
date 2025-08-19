0c521a6dccbccc5f485ed932944a8cce0e6797db
Pukhraj Jharotiya <163715385+Student-Pukhraj@users.noreply.github.com>
2025-08-20 00:40:16 +0530

    Update page.js

--- app/[handle]/page.js
+++ app/[handle]/page.js
@@ -38,7 +38,7 @@ if(!item){
     }
     return <div className="flex min-h-screen bg-purple-400 justify-center items-start py-10">
         {item && <div className="photo flex flex-col items-center justify-center gap-4">
-            <Image className="rounded-full w-32" src={item.picture} alt="" />
+            <image className="rounded-full w-32" src={item.picture} alt="" />
             <span className="font-bold text-xl">@{item.handle}</span>
             <span className="desc w-80 text-center">{item.desc}</span>
             <div className="links">
@@ -52,3 +52,4 @@ if(!item){
     </div>
 
 }
+
